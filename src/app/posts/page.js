9daad16675775df37abb38f19969b270a54ebb42
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-05 19:34:09 +0530

    static site generation by "force-cache"

---
+++ src/app/posts/page.js
@@ -0,0 +1,27 @@
+const page = async () => {
+  const res = await fetch("http://localhost:5000/posts",{
+    cache:"force-cache"
+  });
+  const posts = await res.json();
+  // console.log(posts);
+
+  return (
+    <div className="w-full">
+      <h1 className="text-2xl text-center">Total postss : {posts.length}</h1>
+      {posts.map((post) => (
+        <div key={post.id} className="card bg-base-100 my-5 shadow-xl w-1/2 mx-auto">
+          <div className="card-body">
+            <h2 className="card-title">{post.title}</h2>
+            <p>{post.description}</p>
+            <p>Likes : {post.likes_count}</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">See More</button>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default page;
