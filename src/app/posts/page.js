961ff25f5c8f36a2d76d93b1e3f7ee7f08a5a6b5
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-12-05 23:17:12 +0530

    dynamic data fetching with ssg

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -1,25 +1,32 @@
+import Link from "next/link";
+
 const page = async () => {
-  const res = await fetch("http://localhost:5000/posts",{
+  const res = await fetch("http://localhost:5000/posts", {
     // cache:"force-cache"
-   /*  next:{
+    /*  next:{
       revalidate:5
     } */
-    cache:"no-store"
+    cache: "no-store",
   });
   const posts = await res.json();
   // console.log(posts);
 
   return (
     <div className="w-full">
-      <h1 className="text-2xl text-center">Total postss: {posts.length}</h1>
+      <h1 className="text-2xl text-center">Total posts: {posts.length}</h1>
       {posts.map((post) => (
-        <div key={post.id} className="card bg-base-100 my-5 shadow-xl w-1/2 mx-auto">
+        <div
+          key={post.id}
+          className="card bg-base-100 my-5 shadow-xl w-1/2 mx-auto"
+        >
           <div className="card-body">
             <h2 className="card-title">{post.title}</h2>
             <p>{post.description}</p>
             <p>Likes : {post.likes_count}</p>
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">See More</button>
+              <Link href={`/posts/${post.id}`}>
+                <button className="btn btn-primary">See More</button>
+              </Link>
             </div>
           </div>
         </div>
